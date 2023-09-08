04b00d8fbf242ac6112332ec299d4bb822629e19
raychlcodmakerr <raphaelmatta2007@gmail.com>
2023-09-08 14:52:09 -0400

    test

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
+const maxX = 1000;
+const maxY = 500;
+
+
+
 var canvas = document.getElementById("c")
 var ctx = canvas.getContext("2d");
-ctx.fillRect(50,50,50,50)
+ctx.imageSmoothingEnabled = false;
+canvas.width = maxX;
+canvas.height = maxY;
+ctx.fillRect(100, 100, 100, 100)
+
+
+for (let i = 0; i <= maxX; i+= maxX/5) {
+    ctx.fillRect(i,50,25,25)
+
+}
+ctx.fillRect(maxX-10,maxY-10,50,50)
