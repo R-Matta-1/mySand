6ea47f4e7189af2a3027487d394d17a08a16d9ed
raychlcodmakerr <raphaelmatta2007@gmail.com>
2023-09-10 15:18:15 -0400

    i just.. dislike schoo;

--- script.js
+++ script.js
@@ -5,6 +5,9 @@ const maxY = 500;
 
 var canvas = document.getElementById("c")
 var ctx = canvas.getContext("2d");
+
+canvas.addEventListener("mousemove",)
+
 ctx.imageSmoothingEnabled = false;
 canvas.width = maxX;
 canvas.height = maxY;
@@ -13,15 +16,25 @@ ctx.fillRect(100, 100, 100, 100)
 class particle {
 
     constructor() {
+        this.x = x;
+        this.y = y;
+        this.color = color;
+        this.type = type;
+        
 
+    }
+    act() {
 
+    }
+    draw() {
+        ctx.fillStyle = color;
+        ctx.fillRect(x,y,particleSize,particleSize)
     }
 
 
 
-
 }
-
+ 
 
 for (let i = 0; i <= maxX; i+= maxX/5) {
     ctx.fillRect(i,50,25,25)
