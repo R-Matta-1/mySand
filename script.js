0e43632ab50e17c3d66647f3a05306b66a85adfe
raychlcodmakerr <raphaelmatta2007@gmail.com>
2023-09-10 22:07:47 -0400

    god i hate school

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
-const maxX = 1000;
-const maxY = 500;
 const particleSize = 5;
+const maxX = 1000 - particleSize;
+const maxY = 500 - particleSize;
 
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
 
 var canvas = document.getElementById("c")
 var ctx = canvas.getContext("2d");
@@ -21,24 +29,33 @@ canvas.addEventListener("mousemove",mouseMove)
 
 class particle {
 
-    constructor() {
+    constructor(x,y,color,type) {
         this.x = x;
         this.y = y;
         this.color = color;
         this.type = type;
-        
-
     }
     act() {
 
     }
     draw() {
-        ctx.fillStyle = color;
-        ctx.fillRect(x*particleSize,y*particleSize,particleSize,particleSize)
+        ctx.fillStyle = this.color;
+        ctx.fillRect(this.x*particleSize,this.y*particleSize,particleSize,particleSize)
     }
-
-
-
 }
- 
+particles = [];
+for (let x = 0;  x< (maxX / particleSize); x++) {
+    particles.push([]);
 
+    for (let y = 0; y < (maxY / particleSize); y++) {
+        particles[x].push(new particle(x, y, getRandomColor(), "empty"))
+    }
+}
+console.log(particles.length);
+
+
+for (let x = 0; x < particles.length; x++) {
+    for (let y = 0; y < particles[x].length; y++) {
+        particles[x][y].draw();
+    }
+}
